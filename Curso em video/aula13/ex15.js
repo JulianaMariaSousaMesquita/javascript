e9f72844c27f7d57ce9ffdeb80481ddb71881e5d
Juliana Maria de Sousa Mesquita <mesquita.juliana1995@gmail.com>
2023-06-26 22:05:20 -0300

    :sparkles: feat: aula 13 - ex15 - Estrutura de repetição (While e Do While)

---
+++ Curso em video/aula13/ex15.js
@@ -0,0 +1,14 @@
+var c = 1;
+var i = 1;
+
+console.log(`Estrutura de repetição While:`);
+while(c <= 6){
+ console.log(`Passo c = ${c}`);
+ c++
+}
+
+console.log(`Estrutura de repetição Do While:`);
+do {
+    console.log(`Passo i = ${i}`);
+    i++
+}while(i <= 6)
