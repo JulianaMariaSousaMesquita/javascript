e856a325c3342a2842fa77d4f317cbecbc0cac7a
Juliana Maria de Sousa Mesquita <mesquita.juliana1995@gmail.com>
2023-07-07 23:25:43 -0300

    :sparkles: feat: aula 17 - ex24 - Objeto

---
+++ Curso em video/aula17/ex24.js
@@ -0,0 +1,16 @@
+let amigo = {nome:'José', 
+    sexo: 'M', 
+    peso: 85.4, 
+    engordar(p){ 
+        console.log('Engordou!') 
+        this.peso += p
+    }
+}
+
+console.log(amigo)
+console.log(typeof amigo)
+
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
