5f5e96973041c626c6093a28a8575b31e7cfd830
Juliana Maria de Sousa Mesquita <mesquita.juliana1995@gmail.com>
2023-07-07 21:33:00 -0300

    :sparkles: feat: aula pratica 4 - analisador de números

---
+++ Curso em video/aulaPratica/aulaPratica04/script.js
@@ -0,0 +1,56 @@
+var vetor = []
+var result = window.document.getElementById('result')
+var fim = window.document.getElementById('finalizar')
+
+function media(vet){
+    let valor = 0
+    for(val in vet){
+        valor += vet[val]
+    }
+    return (valor/(vet.length))
+}
+
+function soma(vet){
+    let valor = 0
+    for(val in vet){
+        valor += vet[val]
+    }
+    return valor
+}
+
+function add(){    
+    let v = Number(window.document.getElementById('val').value)
+    if(v >= 1 && v <= 100){
+        if(vetor.indexOf(v) === -1){
+            vetor.push(v)
+            result.innerHTML += `<option value="${v}">Valor ${v} adicionado</option>`
+        }else{
+            alert(`O valor ${v} já esta cadastrado, digite outro valor porfavor!`)
+        }
+    }else{
+        alert('Digite valor entre 1 e 100')
+    }
+}
+
+function finalizar(){
+    fim.innerHTML = ''
+    if( vetor.length > 0){
+        vetor.sort()
+        if(vetor.indexOf(100) !== -1){
+            vetor.splice((vetor.indexOf(100)), 1);
+            vetor.push(100)
+        }
+        fim.innerHTML += `Ao todo, temos ${vetor.length} números cadastrados. <BR/>
+        O maior valor informado foi ${vetor[vetor.length - 1]}. <BR/>
+        O menor valor informado foi ${vetor[0]}. <BR/>
+        Somando todos os valores, temos ${soma(vetor)}. <BR/>
+        A média dos valores digitados é ${media(vetor)}.`
+    }else{
+        alert(" [ERROR] Adicione valores entre 1 e 100 antes de finalizar")
+    }
+}
+
+function limpar(){
+    result.innerHTML = ''
+    vetor = []
+}
