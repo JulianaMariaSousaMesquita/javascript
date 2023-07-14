c980586ca7f5f7eeddf9cfe53fe3709108bba548
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2023-07-14 00:16:33 -0300

    :sparkles: feat: aula 05 - Operadores relacionais

---
+++ CFBcursos/aula05/script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+let num1 = 10, num2 = 5, num3 = 10, num4 = "10"
+
+console.log(`num1: ${num1}, num2: ${num2}, num3: ${num3}, num4: ${num4}`)
+
+console.log("num1 > num2 = "+(num1 > num2))
+console.log("num1 > num3 = "+(num1 > num3))
+console.log("num1 > num4 = "+(num1 > num4))
+
+console.log("num1 < num2 = "+(num1 < num2))
+console.log("num1 < num3 = "+(num1 < num3))
+console.log("num1 < num4 = "+(num1 < num4))
+
+console.log("num1 >= num3 = "+(num1 >= num3))
+console.log("num1 >= num2 = "+(num1 >= num2))
+console.log("num1 >= num4 = "+(num1 >= num4))
+
+console.log("num1 == num2 = "+(num1 == num2))
+console.log("num1 === num2 = "+(num1 === num2))
+console.log("num1 == num3 = "+(num1 == num3))
+console.log("num1 === num3 = "+(num1 === num3))
+console.log("num1 == num4 = "+(num1 == num4))
+console.log("num1 === num4 = "+(num1 === num4))
+
+console.log("num1 != num3 = "+(num1 != num3))
+console.log("num1 != num2 = "+(num1 != num2))
+console.log("num1 != num4 = "+(num1 != num4))
+console.log("num1 !== num4 = "+(num1 !== num4))
+
+console.log("!(num1 === num4) = "+!(num1 === num4))
+console.log("!(num1 > num3) = "+!(num1 > num3))
