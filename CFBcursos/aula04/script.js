f6d72ed5decd5a70b53a6608d42da7446dd2205e
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2023-07-13 23:13:51 -0300

    :sparkles: feat: aula 04 - Operadores matemáticos

---
+++ CFBcursos/aula04/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let num1 = 0, num2=0, res=0
+num1 = 2
+num2 = 15
+res=num1+num2
+console.log(` num1: ${num1}, num2: ${num2}, res: ${res}`)
+console.log("res = "+res)
+
+console.log("num2/num1 = "+(num2/num1))
+console.log("num2%num1 = "+(num2%num1))
+
+console.log("num1++ = "+num1++)
+console.log("num1 = "+num1)
+console.log("num1+=1 = "+(num1+=1))
+console.log("++num1 = "+(++num1))
+console.log("num1 = "+num1)
+
+console.log("num2-num1 = "+(num2-num1))
+console.log("num1-- = "+num1--)
+console.log("num1 = "+num1)
+console.log("num1-=1 = "+(num1-=1))
+console.log("--num1 = "+(--num1))
+console.log("num1 = "+num1)
+
+
